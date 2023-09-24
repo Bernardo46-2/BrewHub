52d52146d65b0a6098109afe43051e1b4ce30829
Toni Lommez <tonilommez@hotmail.com>
2023-09-24 01:43:32 -0300

    Melhoria que o bernardo nao conseguiu resolver

--- frontend/lib/welcome.dart
+++ frontend/lib/welcome.dart
@@ -28,89 +28,126 @@ class _WelcomePage extends State<WelcomePage> {
               fit: BoxFit.cover,
               width: MediaQuery.of(ctx).size.width,
               height: MediaQuery.of(ctx).size.height,
-            )
+            ),
           ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    const SizedBox(width: 25),
-                    Stack(
-                      children: <Widget>[
-                        const CircleAvatar(
-                          backgroundColor: primary6,
-                          radius: 45
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 7),
-                          child: Image.asset(
-                            'assets/brewhub_logo.png',
-                            width: 80,
-                            height: 80,
-                            fit: BoxFit.contain
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              // Logo Section
+              Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 40.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Stack(
+                        children: <Widget>[
+                          const CircleAvatar(
+                            backgroundColor: primary6,
+                            radius: 45,
                           ),
-                        )
-                      ]
-                    ),
-                    const SizedBox(width: 30),
-                    Image.asset(
-                      'assets/brewhub.png',
-                      width: 220,
-                      height: 220,
-                      fit: BoxFit.contain
-                    ),
-                  ],
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size(componentsWidth, 40),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)
-                          ),
-                        ),
-                        onPressed: () {
-                          Navigator.of(ctx).push(
-                            MaterialPageRoute(
-                              builder: (ctx) => const LoginPage(),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 7),
+                            child: Image.asset(
+                              'assets/brewhub_logo.png',
+                              width: 80,
+                              height: 80,
+                              fit: BoxFit.contain,
                             ),
-                          );
-                        },
-                        child: const Text('Login'),
+                          ),
+                        ],
                       ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: dark3,
-                          minimumSize: const Size(componentsWidth, 40),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)
-                          ),
-                        ),
-                        onPressed: () {
-                          Navigator.of(ctx).push(
-                            MaterialPageRoute(
-                              builder: (ctx) => const RegisterPage(),
-                            ),
-                          );
-                        },
-                        child: const Text('Register'),
+                      const SizedBox(width: 30),
+                      Image.asset(
+                        'assets/brewhub.png',
+                        width: 220,
+                        height: 220,
+                        fit: BoxFit.contain,
                       ),
                     ],
                   ),
-                )
-              ],
-            ),
-          ), 
-        ]
-      )
+                ),
+              ),
+              
+              // Espaço entre o logo e os textos/botões
+              Expanded(child: Container()),
+
+              // Texts and Buttons Section
+              Padding(
+                padding: const EdgeInsets.only(bottom: 30),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      width: componentsWidth,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'Bem-vindo (ou vinda) ao BrewHub',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            'Seu café virtual para conexões reais. Trabalhe, relaxe e conecte-se. Entre e faça parte da nossa comunidade!',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Color.fromARGB(128, 255, 255, 255)),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(componentsWidth, 40),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.of(ctx).push(
+                          MaterialPageRoute(
+                            builder: (ctx) => const LoginPage(),
+                          ),
+                        );
+                      },
+                      child: const Text('Login'),
+                    ),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: dark3,
+                        minimumSize: const Size(componentsWidth, 40),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.of(ctx).push(
+                          MaterialPageRoute(
+                            builder: (ctx) => const RegisterPage(),
+                          ),
+                        );
+                      },
+                      child: const Text('Register'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
